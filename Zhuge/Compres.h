143af0ba960d6a746e270f9cc79935c7a49b6b4d
jiaokang <kangjiao@37degree.com>
2015-12-02 12:16:30 +0800

    上传两步校验

---
+++ Zhuge/Compres.h
@@ -0,0 +1,17 @@
+//
+//  Compres.h
+//  HelloZhuge
+//
+//  Created by jiaokang on 15/7/27.
+//  Copyright (c) 2015年 37degree. All rights reserved.
+//
+#import "zlib.h"
+#import <Foundation/Foundation.h>
+
+@interface NSData (NSDataExtension)
+
+// ZLIB
+
+- (NSData *) zlibDeflate;
+
+@end
