c215429d177f05a838e09070c642d3e96a65f0d1
jiaokang <jiaokang@zhugeio.com>
2017-06-20 11:56:47 +0800

    update version to 3.0.2_b

--- HelloZhuge/HelloZhuge/Zhuge/ZhugeConfig.h
+++ HelloZhuge/HelloZhuge/Zhuge/ZhugeConfig.h
@@ -7,7 +7,7 @@
 #import <Foundation/Foundation.h>
 
 /* SDK版本 */
-#define ZG_SDK_VERSION @"3.0.0_b"
+#define ZG_SDK_VERSION @"3.0.2_b"
 
 /* 默认应用版本 */
 #define ZG_APP_VERSION [[[NSBundle mainBundle] infoDictionary] objectForKey:@"CFBundleShortVersionString"]
